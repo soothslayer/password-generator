78754606ab4b1655ef43336da6c36e84def58c7e
Hanlon Miller <hanlonmiller@yahoo.com>
2023-05-21 07:30:14 -0400

    intial commit. basic working structure

--- Password Generator Extension/Resources/popup.js
+++ Password Generator Extension/Resources/popup.js
@@ -1,1 +1,83 @@
 console.log("Hello World!", browser);
+function copyToClipboard() {
+    document.getElementById("passwordField").select();
+    document.execCommand('copy');
+    document.getElementById("passwordField").blur()
+}
+document.getElementById("passwordField").value = generatePassword(document.getElementById("rangePasswordLength").value);
+copyToClipboard();
+document.querySelector("#rangePasswordLength").addEventListener('change', () => {
+    document.getElementById("passwordLengthNumber").value = document.getElementById("rangePasswordLength").value
+    document.getElementById("passwordField").value = generatePassword(document.getElementById("rangePasswordLength").value);
+    copyToClipboard();
+});
+document.querySelector("#rangePasswordLength").addEventListener('input', () => {
+    document.getElementById("passwordLengthNumber").value = document.getElementById("rangePasswordLength").value
+    document.getElementById("passwordField").value = generatePassword(document.getElementById("rangePasswordLength").value);
+    copyToClipboard();
+});
+document.querySelector("#passwordLengthNumber").addEventListener('change', () => {
+    document.getElementById("rangePasswordLength").value = document.getElementById("passwordLengthNumber").value
+    document.getElementById("passwordField").value = generatePassword(document.getElementById("rangePasswordLength").value);
+    copyToClipboard();
+});
+document.querySelector("#passwordLengthNumber").addEventListener('input', () => {
+    document.getElementById("rangePasswordLength").value = document.getElementById("passwordLengthNumber").value
+    document.getElementById("passwordField").value = generatePassword(document.getElementById("rangePasswordLength").value);
+    copyToClipboard();
+});
+document.querySelector("#passwordWithNumbers").addEventListener('click', () => {
+    //document.getElementById("rangePasswordLength").value = document.getElementById("passwordLengthNumber").value
+    document.getElementById("passwordField").value = generatePassword(document.getElementById("rangePasswordLength").value);
+    copyToClipboard();
+});
+document.querySelector("#passwordWithUppercase").addEventListener('click', () => {
+    //document.getElementById("rangePasswordLength").value = document.getElementById("passwordLengthNumber").value
+    document.getElementById("passwordField").value = generatePassword(document.getElementById("rangePasswordLength").value);
+    copyToClipboard();
+});
+document.querySelector("#passwordWithSymbols").addEventListener('click', () => {
+    //document.getElementById("rangePasswordLength").value = document.getElementById("passwordLengthNumber").value
+    document.getElementById("passwordField").value = generatePassword(document.getElementById("rangePasswordLength").value);
+    copyToClipboard();
+});
+function updateNumberInput(newVal){
+    document.getElementById("passwordLengthNumber").value=newVal;
+}
+function generatePassword(length) {
+    let charset = "abcdefghijklmnopqrstuvwxyz"
+    let includeNumbers = document.getElementById("passwordWithNumbers").checked
+    if (includeNumbers) {
+        charset += "0123456789"
+    } else {
+        charset = charset.replace(/[0-9]/g, '');
+    }
+    let includeUppercase = document.getElementById("passwordWithUppercase").checked
+    if (includeUppercase) {
+        charset += "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    } else {
+        charset = charset.replace(/[A-Z]/g, '');
+    }
+    let includeSymbols = document.getElementById("passwordWithSymbols").checked
+    if (includeSymbols) {
+        charset += "!@#$%^&*()_+"
+    } else {
+        charset = charset.replace(/[!@#$%^&*()_+]/g, '');
+    }
+  //const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+";
+  let password = "";
+  for (let i = 0; i < length; i++) {
+    password += charset.charAt(Math.floor(Math.random() * charset.length));
+  }
+  return password;
+}
+document.getElementById("generatePasswordButton").addEventListener("click", function() {
+    let passwordLength = document.getElementById("rangePasswordLength").value
+  const password = generatePassword(passwordLength); // Generate a password of length 12
+  document.getElementById("passwordField").value = password; // Display the password in an input field
+    copyToClipboard();
+});
+document.getElementById("passwordField").addEventListener("click", function() {
+    this.select()
+    document.execCommand('copy');
+});
--- Password Generator Extension/SafariWebExtensionHandler.swift
+++ Password Generator Extension/SafariWebExtensionHandler.swift
@@ -7,7 +7,22 @@
 
 import SafariServices
 import os.log
-
+import Foundation
+import SafariServices
+/*
+class PasswordGenerator {
+    static func generatePassword(length: Int) -> String {
+        let charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+"
+        var password = ""
+        for _ in 0..<length {
+            let index = Int(arc4random_uniform(UInt32(charset.count)))
+            let character = charset.randomElement()
+            password += character
+        }
+        return password
+    }
+}
+*/
 class SafariWebExtensionHandler: NSObject, NSExtensionRequestHandling {
 
     func beginRequest(with context: NSExtensionContext) {
