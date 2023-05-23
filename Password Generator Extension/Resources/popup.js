433632c1703935ef4411dcc6e9375dd722d24bdb
Hanlon Miller <hanlonmiller@yahoo.com>
2023-05-22 21:33:54 -0400

    added options

--- Password Generator Extension/Resources/popup.js
+++ Password Generator Extension/Resources/popup.js
@@ -1,4 +1,19 @@
 console.log("Hello World!", browser);
+function saveOptions() {
+  browser.storage.sync.set({
+    rangePasswordLength: document.querySelector("#rangePasswordLength").value
+    //passwordWithNumbers:   document.querySelector("#passwordWithNumbers").checked
+    //passwordWithUppercase: document.querySelector("#passwordWithUppercase").checked
+    //passwordWithSymbols: document.querySelector("#passwordWithSymbols").checked
+  });
+}
+function restoreOptions() {
+    let storageItemrangePasswordLength = browser.storage.managed.get('rangePasswordLength');
+    storageItemrangePasswordLength.then((res) => {
+        document.querySelector("#rangePasswordLength").value = res.rangePasswordLength;
+    });
+}
+document.addEventListener('DOMContentLoaded', restoreOptions);
 function copyToClipboard() {
     document.getElementById("passwordField").select();
     document.execCommand('copy');
@@ -6,40 +21,49 @@ function copyToClipboard() {
 }
 document.getElementById("passwordField").value = generatePassword(document.getElementById("rangePasswordLength").value);
 copyToClipboard();
+
 document.querySelector("#rangePasswordLength").addEventListener('change', () => {
     document.getElementById("passwordLengthNumber").value = document.getElementById("rangePasswordLength").value
     document.getElementById("passwordField").value = generatePassword(document.getElementById("rangePasswordLength").value);
     copyToClipboard();
+    saveOptions();
+    
 });
 document.querySelector("#rangePasswordLength").addEventListener('input', () => {
     document.getElementById("passwordLengthNumber").value = document.getElementById("rangePasswordLength").value
     document.getElementById("passwordField").value = generatePassword(document.getElementById("rangePasswordLength").value);
     copyToClipboard();
+    saveOptions();
 });
 document.querySelector("#passwordLengthNumber").addEventListener('change', () => {
     document.getElementById("rangePasswordLength").value = document.getElementById("passwordLengthNumber").value
     document.getElementById("passwordField").value = generatePassword(document.getElementById("rangePasswordLength").value);
     copyToClipboard();
+    saveOptions();
 });
 document.querySelector("#passwordLengthNumber").addEventListener('input', () => {
     document.getElementById("rangePasswordLength").value = document.getElementById("passwordLengthNumber").value
     document.getElementById("passwordField").value = generatePassword(document.getElementById("rangePasswordLength").value);
     copyToClipboard();
+    saveOptions();
 });
 document.querySelector("#passwordWithNumbers").addEventListener('click', () => {
     //document.getElementById("rangePasswordLength").value = document.getElementById("passwordLengthNumber").value
     document.getElementById("passwordField").value = generatePassword(document.getElementById("rangePasswordLength").value);
     copyToClipboard();
+    saveOptions();
 });
 document.querySelector("#passwordWithUppercase").addEventListener('click', () => {
     //document.getElementById("rangePasswordLength").value = document.getElementById("passwordLengthNumber").value
     document.getElementById("passwordField").value = generatePassword(document.getElementById("rangePasswordLength").value);
     copyToClipboard();
+    saveOptions();
 });
 document.querySelector("#passwordWithSymbols").addEventListener('click', () => {
     //document.getElementById("rangePasswordLength").value = document.getElementById("passwordLengthNumber").value
     document.getElementById("passwordField").value = generatePassword(document.getElementById("rangePasswordLength").value);
     copyToClipboard();
+    saveOptions();
 });
 function updateNumberInput(newVal){
     document.getElementById("passwordLengthNumber").value=newVal;
